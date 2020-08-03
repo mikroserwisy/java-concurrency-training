cc930a34f17903b4ed8c655593400c8342330afb
landrzejewski <landrzejewski.poczta@gmail.com>
2020-08-03 16:52:39 +0200

    Update

---
+++ src/main/java/pl/training/concurrency/ex013_semaphore/Application.java
@@ -0,0 +1,17 @@
+package pl.training.concurrency.ex013_semaphore;
+
+import java.util.UUID;
+import java.util.concurrent.Semaphore;
+
+public class Application {
+    private static final int THREADS_COUNT = 20;
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(5);
+        PrintingQueue printingQueue = new PrintingQueue(semaphore);
+        for (int threadIndex = 0; threadIndex < THREADS_COUNT; threadIndex++) {
+            new Thread(() -> printingQueue.print(UUID.randomUUID().toString())).start();
+        }
+    }
+
+}
---
+++ src/main/java/pl/training/concurrency/ex013_semaphore/PrintingQueue.java
@@ -0,0 +1,34 @@
+package pl.training.concurrency.ex013_semaphore;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PrintingQueue {
+
+    private static final int MAX_TIMEOUT = 10_000;
+
+    private final Random random = new Random();
+    private final Semaphore semaphore;
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    public PrintingQueue(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    public void print(String text) {
+        try {
+            logger.log(Level.INFO, "Thread " + Thread.currentThread().getName() + " is waiting for lock");
+            semaphore.acquire();
+            logger.log(Level.INFO, "Thread " + Thread.currentThread().getName() + " acquired lock");
+            Thread.sleep(random.nextInt(MAX_TIMEOUT));
+            System.out.println(text);
+            semaphore.release();
+            logger.log(Level.INFO, "Thread " + Thread.currentThread().getName() + " released lock");
+        } catch (InterruptedException e) {
+            logger.log(Level.INFO, "Thread interrupted: " + e.getMessage());
+        }
+    }
+
+}
