2674e243bdb65bfd5a27550efb91042d001c852a
landrzejewski <landrzejewski.poczta@gmail.com>
2020-08-04 10:14:54 +0200

    Update

---
+++ src/main/java/pl/training/concurrency/solution01/Application.java
@@ -0,0 +1,41 @@
+package pl.training.concurrency.solution01;
+
+public class Application {
+
+    public static void main( String args[] ) throws InterruptedException {
+        Bathroom bathroom = new Bathroom();
+        Thread female1 = asyncRun(() -> bathroom.femaleUseBathroom("Anna"));
+        Thread male1 = asyncRun(() -> bathroom.maleUseBathroom("Jan"));
+        Thread male2 = asyncRun(() -> bathroom.maleUseBathroom("Marek"));
+        Thread male3 = asyncRun(() -> bathroom.maleUseBathroom("Adam"));
+        Thread male4 = asyncRun(() -> bathroom.maleUseBathroom("Michał"));
+
+        female1.start();
+        male1.start();
+        male2.start();
+        male3.start();
+        male4.start();
+        female1.join();
+        male1.join();
+        male2.join();
+        male3.join();
+        male4.join();
+    }
+
+    interface Task {
+
+        void run() throws InterruptedException;
+
+    }
+
+    private static Thread asyncRun(Task task) {
+        return new Thread(() -> {
+            try {
+                task.run();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+}
---
+++ src/main/java/pl/training/concurrency/solution01/Bathroom.java
@@ -0,0 +1,63 @@
+package pl.training.concurrency.solution01;
+
+import java.util.concurrent.Semaphore;
+
+public class Bathroom {
+
+    private enum State {
+
+        WOMAN, MEN, NONE
+
+    }
+
+    private final Semaphore maxEmployees = new Semaphore(3);
+    private State inUseBy = State.NONE;
+    private int employeesInBathroom = 0;
+
+    public void maleUseBathroom(String name) throws InterruptedException {
+        synchronized (this) {
+            while (inUseBy == State.WOMAN) {
+                this.wait();
+            }
+            maxEmployees.acquire();
+            employeesInBathroom++;
+            inUseBy = State.MEN;
+        }
+        enterBathroom(name);
+        maxEmployees.release();
+        synchronized (this) {
+            employeesInBathroom--;
+            if (employeesInBathroom == 0) {
+                inUseBy = State.NONE;
+            }
+            this.notifyAll();
+        }
+    }
+
+    public void femaleUseBathroom(String name) throws InterruptedException {
+        synchronized (this) {
+            while (inUseBy == State.MEN) {
+                this.wait();
+            }
+            maxEmployees.acquire();
+            employeesInBathroom++;
+            inUseBy = State.WOMAN;
+        }
+        enterBathroom(name);
+        maxEmployees.release();
+        synchronized (this) {
+            employeesInBathroom--;
+            if (employeesInBathroom == 0) {
+                inUseBy = State.NONE;
+            }
+            this.notifyAll();
+        }
+    }
+
+    private void enterBathroom(String name) throws InterruptedException {
+        System.out.println(name + " is using bathroom. Employees in bathroom = " + employeesInBathroom);
+        Thread.sleep(1_000);
+        System.out.println(name + " is leaving bathroom ");
+    }
+
+}
