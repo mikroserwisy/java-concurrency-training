f5ec9f563f39a5a5b5b50fec275f67419ffce24c
landrzejewski <landrzejewski.poczta@gmail.com>
2020-08-04 09:33:49 +0200

    Update

---
+++ src/main/java/pl/training/concurrency/ex006_read_write_lock/ReadWriteLock.java
@@ -0,0 +1,32 @@
+package pl.training.concurrency.ex006_read_write_lock;
+
+public class ReadWriteLock {
+
+    private boolean isWriteLocked = false;
+    private int readers = 0;
+
+    public synchronized void acquireReadLock() throws InterruptedException {
+        while (isWriteLocked) {
+            wait();
+        }
+        readers++;
+    }
+
+    public synchronized void releaseReadLock() {
+        readers--;
+        notify();
+    }
+
+    public synchronized void acquireWriteLock() throws InterruptedException {
+        while (isWriteLocked || readers != 0) {
+            wait();
+        }
+        isWriteLocked = true;
+    }
+
+    public synchronized void releaseWriteLock() {
+        isWriteLocked = false;
+        notify();
+    }
+
+}
